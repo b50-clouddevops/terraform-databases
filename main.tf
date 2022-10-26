687da3d2572f09cd8757fba4b597dd48fe6d4072
mvpc-11232 <mvp2612@hotmail.com>
2022-10-26 07:28:03 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ module "mongodb" {
   source              = "./vendor/modules/mongodb"
   ENV                 = var.ENV
 }
+
+module "redis" {
+  source              = "./vendor/modules/redis"
+  ENV                 = var.ENV
+}
+
