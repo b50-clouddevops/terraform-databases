c0724cc1fecb573b2010aac405f68bdcedca4c4e
mvpc-11232 <mvp2612@hotmail.com>
2022-10-26 06:18:42 +0530

    Auto Push Commit

--- vars.tf
+++
@@ -1,9 +0,0 @@
-variable "VPC_CIDR" {}
-variable "PUBLIC_SUBNET_CIDR" {}
-variable "PRIVATE_SUBNET_CIDR" {}
-variable "ENV" {}
-variable "AZ" {}
-variable "DEFAULT_VPC_ID" {}
-variable "DEFAULT_VPC_CIDR" {}
-variable "DEFAULT_VPC_RT" {}
-
